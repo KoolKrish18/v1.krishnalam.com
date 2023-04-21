02d305db3758bae3f7afcb61e366d02652573464
Krish Nalam <nalamkrish1@gmail.com>
2023-04-21 10:32:58 -0400

    Added StartUp Page

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-var tablinks = document.getElementsByClassName('tab-links');
-var tabcontents = document.getElementsByClassName('tab-contents');
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
-  for (tablink of tablinks) {
-    tablink.classList.remove('active-link');
-  }
-  for (tabcontent of tabcontents) {
-    tabcontent.classList.remove('active-tab');
-  }
-  event.currentTarget.classList.add('active-link');
-  document.getElementById(tabname).classList.add('active-tab');
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
-var sidemenu = document.getElementById('sidemenu');
+var splashScreen = document.querySelector(".splash");
+splashScreen.addEventListener("click", () => {
+    splashScreen.style.opacity = 0;
+    setTimeout(() => {
+        splashScreen.classList.add("hidden");
+    }, 610);
 
-function openmenu() {
-  sidemenu.style.right = '0';
-}
-function closemenu() {
-  sidemenu.style.right = '-200px';
-}
-
-const output = document.getElementById('txt');
-let i = 0;
-let currentPara = 0;
-const x = `Hi, I\'m <nl>
+    const output = document.getElementById("txt");
+    let i = 0;
+    let currentPara = 0;
+    const x = `Hi, I\'m <nl>
 Krish Nalam`;
 
-const splitted = x.split('<nl>');
+    const splitted = x.split("<nl>");
 
-splitted.forEach(function (text, index) {
-  const p = document.createElement('p');
-  output.appendChild(p);
-  if (index === 1) {
-    p.className = 'nameColor';
-  }
+    splitted.forEach(function (text, index) {
+        const p = document.createElement("p");
+        output.appendChild(p);
+        if (index === 1) {
+            p.className = "nameColor";
+        }
+    });
+    const paras = output.querySelectorAll("p");
+
+    const intervalId = setInterval(function () {
+        paras[currentPara].innerHTML += splitted[currentPara][i];
+        i++;
+        if (i === splitted[currentPara].length) {
+            currentPara++;
+            i = 0;
+            if (currentPara === splitted.length) {
+                clearInterval(intervalId);
+            }
+        }
+    }, 150);
 });
-const paras = output.querySelectorAll('p');
-
-const intervalId = setInterval(function () {
-  paras[currentPara].innerHTML += splitted[currentPara][i];
-  i++;
-  if (i === splitted[currentPara].length) {
-    currentPara++;
-    i = 0;
-    if (currentPara === splitted.length) {
-      clearInterval(intervalId);
-    }
-  }
-}, 75);
